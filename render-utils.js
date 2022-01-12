4d0fa1e70796cc21dbfb9438e1526c0341a8e938
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-01-12 15:46:09 -0800

    test passes

--- render-utils.js
+++ render-utils.js
@@ -1,5 +1,5 @@
 export async function renderParticipant(participant) {
-    const participantEl = document.createElement('h3');
+    const participantEl = document.createElement('p');
     participantEl.classList.add('participant');
     participantEl.textContent = `${participant.name}: ${participant.contact_email}`;
 
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,25 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { renderParticipant } from '../render-utils.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderParticipant(participant) should return a DOM node', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const testParticipant = {
+        name: 'Tilly',
+        contact_email: 'test@testy.com'
+    };
+
+    const expected = 'Tilly: test@testy.com';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderParticipant(testParticipant);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected.outerHTML);
 });
